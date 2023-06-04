38ea1b4524cef9efaf9dfb760110caac4c0f1751
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-06-04 12:03:02 +0300

    Improved styling, added props

--- src/pages/Content/components/Card.jsx
+++ src/pages/Content/components/Card.jsx
@@ -8,20 +8,20 @@ const Card = (props) => {
                         <img
                             src="https://scontent.frix7-1.fna.fbcdn.net/v/t45.5328-4/328285377_8876761889064007_3666937959392068286_n.jpg?stp=c0.0.750.750a_dst-jpg&_nc_cat=107&ccb=1-7&_nc_sid=c48759&_nc_ohc=GwHbg0bn4jsAX-4KfL7&_nc_ht=scontent.frix7-1.fna&oh=00_AfD8MvGn6m5PRP5jluq1qZtOBW-6aQiciS3vGpZFaKWyEw&oe=6480D755"
                             alt="product-image"
-                            className="hover:opacity-75 transition ease-in-out duration-150"
+                            className="rounded-md hover:opacity-75 transition ease-in-out duration-150"
                         />
                     </a>
                 </div>
                 <div className="mt-4">
-                    <a href="#" className="text-lg font-bold hover:text-gray-300">
-                        Product title
+                    <a href="#" className="text-sky-700 hover:decoration-sky-600">
+                        <span className="text-sky-700 text-lg font-bold hover:text-sky-600">{props.title}</span>
                     </a>
                 </div>
-                <div className="flex items-center text-gray-400 space-x-2 text-sm mt-1">
+                <div className="flex items-center space-x-2 text-sm mt-1">
                     <div className="flex space-x-1">
-                        <span>Price</span>
-                        <span>Location</span>
-                        <span>CTA</span>
+                        <span className="text-gray-500">{props.price}</span>
+                        <span className="text-gray-500">{props.location}</span>
+                        <span className="text-gray-500">CTA</span>
                     </div>
                 </div>
             </div>;
--- src/pages/Content/components/Content.jsx
+++ src/pages/Content/components/Content.jsx
@@ -7,12 +7,21 @@ const Content = (props) => {
 
     }, []);
 
-    return <div>
-        <h1 className="bg-red-500">TTTThe product to search in FB: {props.product}</h1>
+    const sampleCardProps = {
+        title: "Product title",
+        price: "500$",
+        location: "Riga",
+    }
+
+    return <div className="mt-4 mb-4 px-4 py-3 rounded-lg shadow-xl space-y-2">
+        <div>
+            <h3 className="text-sky-700">Consider buying {props.product} used</h3>
+            <span className="text-gray-500">Here are some options around you...</span>
+        </div>
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
-            <Card />
-            <Card />
-            <Card />
+            <Card {...sampleCardProps}/>
+            <Card {...sampleCardProps}/>
+            <Card {...sampleCardProps}/>
         </div>
     </div>;
 };
