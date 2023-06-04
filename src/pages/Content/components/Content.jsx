0f50a9fc52094a17e61d044a3151d9102a28d2f3
Dace Flaumane <dacefl@Daces-MacBook-Pro.local>
2023-06-04 15:40:40 +0300

    Geolocation

--- src/pages/Content/components/Content.jsx
+++ src/pages/Content/components/Content.jsx
@@ -1,12 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../../assets/styles/tailwind.css';
 import Card from "./Card";
+import Geolocation from 'react-geolocation';
 
 const Content = (props) => {
-    useEffect(() => {
+    const [location, setLocation] = useState(null);
 
+    useEffect(() => {
+        fetchLocation();
     }, []);
 
+<<<<<<< Updated upstream
     const sampleCardProps = {
         title: "Product title",
         price: "500$",
@@ -18,6 +22,33 @@ const Content = (props) => {
             <h3 className="text-sky-700">Consider buying {props.product} used</h3>
             <span className="text-gray-500">Here are some options around you...</span>
         </div>
+=======
+    const fetchLocation = () => {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const latitude = position.coords.latitude;
+              const longitude = position.coords.longitude;
+              setLocation({ latitude, longitude });
+            },
+            (error) => {
+              console.error(error);
+            }
+          );
+        } else {
+          console.error('Geolocation is not supported by this browser.');
+        }
+      };
+    
+    return <div>
+        <h1 className="bg-red-500">The product to search in FB: {props.product}</h1>
+        {location && (
+            <p className="mt-2">
+            Your location: Latitude - {location.latitude}, Longitude - {location.longitude}
+            </p>
+        )}
+        {!location && <p className="mt-2">Fetching location...</p>}
+>>>>>>> Stashed changes
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
             <Card {...sampleCardProps}/>
             <Card {...sampleCardProps}/>
