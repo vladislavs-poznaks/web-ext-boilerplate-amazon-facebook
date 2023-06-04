a9ee9f79491659662e2fc18179bd275a70cf9a6b
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-06-04 14:51:09 +0300

    WIP

--- src/pages/Content/components/Content.jsx
+++ src/pages/Content/components/Content.jsx
@@ -1,9 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import '../../../assets/styles/tailwind.css';
 import Card from "./Card";
+import axios from "axios";
 
 const Content = (props) => {
+    const [items, setItems] = useState([]);
+
     useEffect(() => {
+
+        // http://localhost:3333/?product=playstation5&latitude=56.9656&longitude=24.0947
+
+        axios({
+            method: 'get',
+            url: `http://localhost:3333/?product=${encodeURIComponent(props.product)}&latitude=56.9656&longitude=24.0947`,
+        })
+            .then((res, err) => {
+                console.log(res, err)
+            });
 
     }, []);
 
