301faa4b1edf112eaac9c807992d35c12320adba
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-06-04 11:27:00 +0300

    Added tailwind

--- src/pages/Content/components/Content.jsx
+++ src/pages/Content/components/Content.jsx
@@ -1,11 +1,14 @@
 import React, {useEffect} from 'react';
+import '../../../assets/styles/tailwind.css';
 
 const Content = (props) => {
     useEffect(() => {
 
     }, []);
 
-    return <h1>TTTThe product to search in FB: {props.product}</h1>;
+    return <div>
+        <h1 className="bg-red-500">TTTThe product to search in FB: {props.product}</h1>
+    </div>;
 };
 
 export default Content;
